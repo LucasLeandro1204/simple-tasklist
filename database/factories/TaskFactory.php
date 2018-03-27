d9ba6348714fe7b31ad479fa9b34a73b8f04fe8b
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-27 19:33:54 -0300

    model

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Task::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->text,
+    ];
+});
