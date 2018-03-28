28af1e08d1ef8c8769fb5ffb4b1f2cc9efda7cc0
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-03-27 21:56:04 -0300

    tasks seeder

---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Task;
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Task::class, 15)->create();
+    }
+}
