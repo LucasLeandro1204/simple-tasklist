d3c2996699b031e88729ed8ec9c0da02c0110bab
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-10-07 02:02:08 -0300

    fetch

--- resources/assets/js/components/Task/Show.vue
+++ resources/assets/js/components/Task/Show.vue
@@ -1,5 +1,21 @@
 <template>
-  <task-section v-if="task" :back="{ name: 'task.index' }">
+  <task-section v-if="fetching" :back="{ name: 'task.index' }">
+    <vue-content-loading slot="header" style="height: 18px" :width="180" :height="18">
+      <rect x="0" y="0" width="180" height="18"></rect>
+    </vue-content-loading>
+
+    <vue-content-loading slot="section-buttons" style="height: 46px" :width="80" :height="46">
+      <rect x="0" y="16" width="180" height="14"></rect>
+    </vue-content-loading>
+
+    <vue-content-loading style="height: 74px" :width="320" :height="74">
+      <rect x="16" y="16" width="288" height="12"></rect>
+      <rect x="16" y="34" width="240" height="12"></rect>
+      <rect x="16" y="52" width="266" height="12"></rect>
+    </vue-content-loading>
+  </task-section>
+
+  <task-section v-else-if="fetched" :back="{ name: 'task.index' }">
     <p slot="header" class="font-bold" v-text="task.title"></p>
 
     <template slot="header-buttons">
@@ -26,14 +42,17 @@
     </div>
   </task-section>
 
-  <task-not-found v-else-if="task === false" />
+  <task-not-found v-else />
 </template>
 
 <script>
   import Marked from 'marked';
+  import Moment from 'moment';
   import TaskSection from '@/Section.vue';
   import TaskNotFound from './NotFound.vue';
   import SectionButton from '@/SectionButton.vue';
+  import VueContentLoading from 'vue-content-loading';
+  import { mapGetters, mapState, mapActions } from 'vuex';
 
   export default {
     props: {
@@ -49,6 +68,11 @@
       TaskSection,
       TaskNotFound,
       SectionButton,
+      VueContentLoading,
+    },
+
+    created () {
+      setTimeout(this.fetch.bind(null, this.id), 1000);
     },
 
     filters: {
@@ -58,6 +82,15 @@
     },
 
     computed: {
+      ...mapState('task', [
+        'task',
+      ]),
+
+      ...mapGetters('task', [
+        'fetched',
+        'fetching',
+      ]),
+
       content () {
         return Marked(this.task.description, {
           sanitize: true,
@@ -66,6 +99,13 @@
     },
 
     methods: {
+      ...mapActions('task', [
+        'fetch',
+      ]),
+
+      ...mapActions('tasks', [
+        'updateStatus',
+      ]),
     },
   };
 </script>
